21847966d29bb80377f03cd7c47083aa083e1092
Miguel Escamilla <mig2190@hotmail.com>
2019-06-26 10:29:55 -0500

    added event listener on button

--- app.js
+++ app.js
@@ -6,18 +6,35 @@ GAME RULES:
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 */
-var scores, roundScore, dice;
+var scores, roundScore;
 
 scores = [0,0]
 roundScore = 0;
 activePlayer = 1;
 
-dice = Math.floor(Math.random() * 6) + 1;
 
-document.querySelector('#current-' + activePlayer).textContent = dice;
+/*document.querySelector('#current-' + activePlayer).textContent = dice;
 document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 
 var x = document.querySelector('#score-0').textContent;
-console.log(x);
+console.log(x);*/
 
 document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+
+	//Random number
+	var dice = Math.floor(Math.random() * 6) + 1;
+
+	//Display the result
+	var diceDOM = document.querySelector('.dice');
+	diceDOM.style.display = 'block';
+	diceDOM.src = 'dice-' + dice + '.png';
+	console.log(dice);
+
+});
